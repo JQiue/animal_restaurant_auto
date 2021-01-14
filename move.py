1d3b0f3ec8c30c51984596d8a3758d58f301a960
Mark4551124015 <Mark4551124015@gmail.com>
2021-01-14 23:48:26 +0800

    Fix move.py
    修改了进入其他餐厅的方式为点击，避免了由于拖动到员工实体上导致翻页失败的BUG

--- move.py
+++ move.py
@@ -1,18 +1,18 @@
 import pyautogui
 
 def toLeft():
-  pyautogui.moveTo(227, 467, 0.5)
-  pyautogui.dragTo(260, 467, 0.3)
+  pyautogui.moveTo(25, 442, 0.5)
+  pyautogui.click()
 
 
 def toRight():
-  pyautogui.moveTo(227, 467, 0.5)
-  pyautogui.dragTo(180, 467, 0.3)
+  pyautogui.moveTo(419, 442, 0.5)
+  pyautogui.click()
 
 def toUp():
-  pyautogui.moveTo(227, 467, 0.5)
-  pyautogui.dragTo(227, 520, 0.3)
+  pyautogui.moveTo(150, 207, 0.5)
+  pyautogui.click()
 
 def toDown():
-  pyautogui.moveTo(227, 467, 0.5)
-  pyautogui.dragTo(227, 420, 0.3)
+  pyautogui.moveTo(166, 800, 0.5)
+  pyautogui.click()
